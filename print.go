cb851434ec126da62bb8deed8064f2e259233a4f
Miguel Filipe <miguel.filipe@gmail.com>
2018-12-10 00:00:24 +0000

    fix bug on print

--- print.go
+++ print.go
@@ -10,17 +10,29 @@ type printElem struct {
 // Print to stdout a Domain Map of URLs in breath-first-order
 func Print(crawled *CrawledDomainMap) {
 	q := []printElem{printElem{page: crawled.Root, indent: 0}}
+	printed := make(map[string]struct{})
 	for len(q) > 0 {
 		curr := q[0]
 		q = q[1:]
-		fmtStr := fmt.Sprintf("%%%ds\n", curr.indent)
-		fmt.Printf(fmtStr, curr.page.URL)
+		if _, ok := printed[curr.page.URL]; ok {
+			continue
+		}
+		printed[curr.page.URL] = struct{}{}
+		fmt.Printf("%s%s\n", indent(curr.indent), curr.page.URL)
 		for _, u := range curr.page.Nodes {
-			fmt.Printf(fmtStr, " -> "+u)
+			fmt.Printf("%s -> %s\n", indent(curr.indent), u)
 			q = append(q, printElem{
 				page:   crawled.URLs[u],
 				indent: curr.indent + 1,
 			})
 		}
 	}
+}
+
+func indent(count int) string {
+	var s string
+	for i := 0; i < count; i++ {
+		s = fmt.Sprintf("  %s", s)
+	}
+	return s
 }
